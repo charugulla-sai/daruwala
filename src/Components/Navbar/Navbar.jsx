3d8af9de9b377ca22e747b6c45ed04acdbf62e70
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-04-05 20:38:45 +0530

    logout functionality implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { useUserContextValues } from '../../Context/UserContext';
 
 function Navbar() {
   const { cartItems } = useCartValues();
-  const { userLoggedIn } = useUserContextValues();
+  const { userLoggedIn, setUserLoggedIn } = useUserContextValues();
   const pathTracker = useLocation().pathname.slice(1);
 
   return (
@@ -23,6 +23,14 @@ function Navbar() {
           <Link className="router_link" to="/cart">
             Cart <span>{cartItems.length}</span>
           </Link>
+          <p
+            className="router_link"
+            onClick={() => {
+              setUserLoggedIn(false);
+            }}
+          >
+            Logout
+          </p>
         </h3>
       )}
     </div>
