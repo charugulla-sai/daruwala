8aa26ef5001b207b69cfdc52b2f689b408e8742f
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-04-09 18:14:12 +0530

    update Header and Navbar components with Responsive styles.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import styles from './Header.module.css';
 import Navbar from '../Navbar/Navbar';
 import { Link } from 'react-router-dom';
-import { useUserContextValues } from '../../Context/UserContext';
 
 function Header() {
-  const { userLoggedIn } = useUserContextValues();
   return (
     <div className={styles.header_component}>
       <div className={styles.header_section}>
         <h1>
-          <Link className="router_link" to='/'>
+          <Link className={styles.router_link} to='/'>
             Daruwala
           </Link>
         </h1>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,30 +10,31 @@ function Navbar() {
 
   return (
     <div className={styles.nav_section}>
-      
       {userLoggedIn ? (
+        <>
+          <h3>
+            <Link className={styles.router_link} to="/cart">
+              Cart <span>{cartItems.length}</span>
+            </Link>
+          </h3>
+          <h3
+            className="router_link"
+            onClick={() => {
+              localStorage.removeItem('auth-token');
+              setUserLoggedIn(localStorage.getItem('auth-token'));
+            }}
+          >
+            Logout
+          </h3>
+        </>
+      ) : (
         <h3>
-          <Link className="router_link" to="/cart">
-            Cart <span>{cartItems.length}</span>
+          <Link
+            className="router_link"
+            to={pathTracker == 'signin' ? 'signup' : 'signin'}
+          >
+            {pathTracker == 'signin' ? 'SignUp' : 'SignIn'}
           </Link>
-        </h3>
-      ):<h3>
-        <Link
-          className="router_link"
-          to={pathTracker == 'signin' ? 'signup' : 'signin'}
-        >
-          {pathTracker == 'signin' ? 'SignUp' : 'SignIn'}
-        </Link>
-      </h3>}
-      {userLoggedIn && (
-        <h3
-          className="router_link"
-          onClick={() => {
-            localStorage.removeItem('auth-token');
-            setUserLoggedIn(localStorage.getItem('auth-token'));
-          }}
-        >
-          Logout
         </h3>
       )}
     </div>
