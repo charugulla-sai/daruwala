a2663054222353ed8188694197d61d84bdc5befb
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-04-13 14:11:51 +0530

    Order section is added in cart component to display the total price of the cart items.; also implemented responsive design for cart items.

---
+++ src/Components/Cart/CartCard/CartCard.jsx
@@ -0,0 +1,57 @@
+import { useCartValues } from '../../../Context/CartContext';
+import { useUserContextValues } from '../../../Context/UserContext';
+import styles from './CartCard.module.css';
+import { useNavigate } from 'react-router-dom';
+
+function CartCard({
+  productId,
+  productImage,
+  productTitle,
+  productPrice,
+  productSize,
+  productType,
+  productQuantity,
+  inCartPage,
+}) {
+  const { addItemToCart, deleteItemFromCart } = useCartValues();
+  const { verify } = useUserContextValues();
+
+  return (
+    <div className={styles.cart_card}>
+      <div className={styles.cart_section}>
+        {/* {deleteItemFromCart && <p className={styles.delete_button}>X</p>} */}
+        <div className={styles.cart_image_box}>
+          <img src={productImage} alt="image"/>
+        </div>
+        <div className={styles.cart_text_box}>
+          <h1 className={styles.cart_product_title}>
+            {productTitle}
+          </h1>
+          <p className={styles.cart_product_type}>{productType}</p>
+          <p className={styles.cart_product_size}>{productSize}ML</p>
+          {productQuantity && <p>qty:{productQuantity}</p>}
+          <div className={styles.price_and_delete_box}>
+            <p className={styles.cart_product_price}>&#x20B9;{productPrice}</p>
+
+            <button
+              onClick={() => {
+                if (verify()) {
+                  if (inCartPage) {
+                    deleteItemFromCart(productId);
+                  } else {
+                    addItemToCart(productId);
+                  }
+                }
+              }}
+              className={styles.add_to_cart}
+            >
+              {inCartPage == true ? 'Remove' : 'Add'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartCard;
--- src/Components/Cart/CartComponent/CartComponent.jsx
+++ src/Components/Cart/CartComponent/CartComponent.jsx
@@ -3,6 +3,7 @@ import ProductCard from '../../Products/ProductsCard/ProductsCard';
 import styles from './CartComponent.module.css';
 import axios from 'axios';
 import { useCartValues } from '../../../Context/CartContext';
+import CartCard from '../CartCard/CartCard';
 
 function CartComponent() {
   const { cartItems } = useCartValues();
@@ -11,10 +12,10 @@ function CartComponent() {
     <div className={styles.cart_component}>
       <div className={styles.cart_section}>
         {cartItems.map((cartProduct) => (
-          <ProductCard
+          <CartCard
             key={cartProduct.image}
             productId={cartProduct._id}
-            productImage={cartProduct.image}
+            productImage={cartProduct.imageUrl}
             productTitle={cartProduct.title}
             productPrice={cartProduct.price}
             productSize={cartProduct.size}
@@ -23,6 +24,31 @@ function CartComponent() {
             inCartPage={true}
           />
         ))}
+      </div>
+      <div className={styles.order_box}>
+        <div className={styles.order_text_box}>
+          <div>
+            <p>Total MRP</p>
+            <p>19,300</p>
+          </div>
+          <div>
+            <p>Discount on MRP</p>
+            <p>-5000</p>
+          </div>
+          <div>
+            <p>Platform Fee</p>
+            <p>Free</p>
+          </div>
+          <div>
+            <p>Shipping fee</p>
+            <p>60</p>
+          </div>
+        </div>
+        <div className={styles.cart_items_total_price}>
+          <p>Total amount</p>
+          <p>2,300</p>
+        </div>
+        <button className={styles.place_order_btn}>Place Order</button>
       </div>
     </div>
   );
