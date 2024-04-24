a72ce319c56229e8cf89c932ab414f4d672a7e97
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-04-25 00:55:40 +0530

    slider buttons added.

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -9,16 +9,19 @@ function Slider() {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentSliderImageIndex((prevValue) => {
-        if (prevValue == 4) {
+        if (prevValue >= 4 && slideDirection === 1) {
           setSlideDirection(-1);
-        } else if (prevValue == 1) {
+          return 5
+        } else if (prevValue <= 1 && slideDirection === -1) {
           setSlideDirection(1);
+          return 0
         }
-        return currentSliderImageIndex + slideDirection;
+        return prevValue + slideDirection;
       });
+      
     }, 5000);
     return () => clearInterval(timer);
-  }, [currentSliderImageIndex]);
+  }, [currentSliderImageIndex, slideDirection]);
 
   return (
     <div className={styles.slide_component}>
@@ -27,16 +30,69 @@ function Slider() {
           <img
             key={index}
             style={{
-              translate: `-${
-                100 * currentSliderImageIndex
-              }%`,
+              translate: `-${100 * currentSliderImageIndex}%`,
             }}
             src={image}
             alt="slider image"
-            
           />
         ))}
       </div>
+      <div
+        className={styles.left_arrow}
+        onClick={() => {
+          setCurrentSliderImageIndex((prevValue) => {
+            // Go to the first image and stop
+            if (prevValue <= 1) {
+              return 0;
+            }
+            return prevValue - 1;
+          });
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
+      </div>
+      {currentSliderImageIndex <= 4 && (
+        <div
+          className={styles.right_arrow}
+          onClick={() => {
+            setCurrentSliderImageIndex((prevValue) => {
+              // Go to the last image and stop
+              if (prevValue >= 4) {
+                return 5;
+              }
+              return prevValue + 1;
+            });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
+      )}
     </div>
   );
 }
